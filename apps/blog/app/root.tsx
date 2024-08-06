c96c86a2836a7182cc22a65b0d548c8fbc220d06
Bruno Santos <brsantos197@gmail.com>
2024-08-05 21:10:13 -0300

    new: vite lang pt-BR

--- apps/blog/app/root.tsx
+++ apps/blog/app/root.tsx
@@ -10,7 +10,7 @@ import "./styles.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
